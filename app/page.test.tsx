15666fda8cc6dfbdd9f834a3cc60b10b406f6d02
Adlin Mohamad Hasri <adlin@rushowl.sg>
2023-10-01 00:56:23 +0700

    first simple page testing

---
+++ app/page.test.tsx
@@ -0,0 +1,16 @@
+import '@testing-library/jest-dom'
+
+import React from 'react'
+import {render, screen} from '@testing-library/react'
+
+import Home from './page'
+
+test("button has correct initial color", () => {
+  render(<Home />)
+
+  // find an element with a role of button and ext of "Change to Blue"
+  const colorButton = screen.getByRole('button', { name: "Change to Blue" })
+
+  // expect the background color to be red
+  expect(colorButton).toHaveStyle({ backgrounColor: 'red' })
+})
