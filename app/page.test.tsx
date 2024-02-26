0071f4955430b12955a441cbd37088f685502287
Adlin Mohamad Hasri <adlin@rushowl.sg>
2024-02-26 11:26:51 +0700

    feat: add act in page.test

--- app/page.test.tsx
+++ app/page.test.tsx
@@ -1,7 +1,7 @@
 import '@testing-library/jest-dom'
 
 import React from 'react'
-import {render, screen} from '@testing-library/react'
+import {act, render, screen} from '@testing-library/react'
 
 import Home from './page'
 
@@ -13,4 +13,17 @@ test("button has correct initial color", () => {
 
   // expect the background color to be red
   expect(colorButton).toHaveStyle({ backgroundColor: 'red' })
+})
+
+test("button clicked and changes the color", () => {
+  render(<Home />)
+
+  // find an element with a role of button and ext of "Change to Blue"
+  const colorButton = screen.getByRole('button', { name: "Change to Blue" })
+  act(() => {
+    /* fire events that update state */
+    colorButton.click()
+  });
+  // expect the background color to be blue
+  expect(colorButton).toHaveStyle({ backgroundColor: 'blue' })
 })
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import React from 'react'
+
 export default function Home() {
+  const [bgColor, setBgColor] = React.useState('red')
+
   return (
     <div className="flex items-center justify-center h-screen">
-      <button style={{ backgroundColor: 'red' }}>
+      <button style={{ backgroundColor: bgColor }} onClick={() => { setBgColor('blue') }}>
         Change to Blue
       </button>
     </div>
